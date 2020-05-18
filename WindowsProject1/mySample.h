7e9dd5452997172ba468d76f03f8454cff52dd60
salmanAltaf101 <sllujaan44@gmail.com>
2020-05-18 14:22:46 +0500

    added project's files.

---
+++ WindowsProject1/mySample.h
@@ -0,0 +1,121 @@
+#pragma once
+
+
+
+void DisplayResourceNAMessageBox(HWND hWnd = NULL)
+{
+    MessageBox(
+        hWnd,
+        (LPCWSTR)L"Resource not available\nDo you want to try again?",
+        (LPCWSTR)L"Account Details",
+        MB_ICONWARNING | MB_CANCELTRYCONTINUE | MB_DEFBUTTON2
+    );
+
+    /*
+    int msgboxID = MessageBox();
+    switch (msgboxID)
+    {
+    case IDCANCEL:
+        // TODO: add code
+        OutputDebugStringW(L"IDCANCEL");
+        break;
+    case IDTRYAGAIN:
+        // TODO: add code
+        OutputDebugStringW(L"IDCANCEL");
+        DisplayResourceNAMessageBox(hWnd);
+        break;
+    case IDCONTINUE:
+        // TODO: add code
+        OutputDebugStringW(L"IDCONTINUE");
+        break;
+    }
+    */
+}
+
+
+
+
+
+
+void DisplayWindowCannotMoveMessageBox(HWND hWnd = NULL)
+{
+    int msgboxID = MessageBox(
+        hWnd,
+        (LPCWSTR)L"Window cannot be centered.",
+        (LPCWSTR)L"Window Move Details",
+        MB_ICONWARNING
+    );
+}
+
+
+
+BOOL CenterWindow(HWND hwndWindow)
+{
+    RECT rectWindow;
+
+    
+        GetWindowRect(hwndWindow, &rectWindow);
+
+        int nWidth = rectWindow.right - rectWindow.left;
+        int nHeight = rectWindow.bottom - rectWindow.top;
+
+
+        int nScreenWidth = GetSystemMetrics(SM_CXSCREEN);
+        int nScreenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+        //screen center
+        int nScreenWidthCenter = nScreenWidth / 2;
+        int nScreenHeightCenter = nScreenHeight / 2;
+
+        //center handwindow
+        int nX = nScreenWidthCenter - (nWidth / 2);
+        int nY = nScreenHeightCenter - (nHeight / 2);
+
+        
+
+
+        MoveWindow(hwndWindow, nX, nY, nWidth, nHeight, FALSE);
+
+        OutputDebugStringW((LPCWSTR)L"window centered");
+
+        return TRUE;
+}
+
+
+
+
+
+BOOL CenterChildWindowRelativeToParent(HWND hwndWindow)
+{
+    HWND hwndParent;
+    RECT rectWindow, rectParent;
+
+    // make the window relative to its parent
+    if ((hwndParent = GetParent(hwndWindow)) != NULL)
+    {
+        GetWindowRect(hwndWindow, &rectWindow);
+        GetWindowRect(hwndParent, &rectParent);
+
+        int nWidth = rectWindow.right - rectWindow.left;
+        int nHeight = rectWindow.bottom - rectWindow.top;
+
+        int nX = ((rectParent.right - rectParent.left) - nWidth) / 2 + rectParent.left;
+        int nY = ((rectParent.bottom - rectParent.top) - nHeight) / 2 + rectParent.top;
+
+        int nScreenWidth = GetSystemMetrics(SM_CXSCREEN);
+        int nScreenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+        // make sure that the dialog box never moves outside of the screen
+        if (nX < 0) nX = 0;
+        if (nY < 0) nY = 0;
+        if (nX + nWidth > nScreenWidth) nX = nScreenWidth - nWidth;
+        if (nY + nHeight > nScreenHeight) nY = nScreenHeight - nHeight;
+
+        MoveWindow(hwndWindow, nX, nY, nWidth, nHeight, FALSE);
+
+        return TRUE;
+    }
+    OutputDebugStringW(L"window had no parent.");
+
+    return FALSE;
+}
---
+++ WindowsProject1/windowStuff.h
@@ -0,0 +1,157 @@
+#pragma once
+
+#include"mySample.h"
+
+#define BUTTON_ID_OK 0
+#define ID_FILE_MENU 1
+#define ID_EXIT_MENU 0x1df
+#define ID_TOOLS_MENU 0x2f0
+#define ID_ABOUT_MENU 0x100
+
+HMENU hMenu;
+
+
+
+void handleAbout(HWND hWnd) {
+    OutputDebugStringW((LPCWSTR)L"handleAbout called.");
+    
+    int msgboxID = MessageBox(
+        hWnd,
+        (LPCWSTR)L"Developed by M.Salman Altaf",
+        (LPCWSTR)L"About",
+        MB_OK
+    );
+    
+}
+
+
+
+void handleMainMenu(HWND hWnd, HMENU hMenuMain) {
+    hMenuMain = CreateMenu();
+    HMENU hFileMenu = CreateMenu();
+    HMENU hSubFileMenu = CreateMenu();
+    HMENU hHelpMenu = CreateMenu();
+    
+    
+    AppendMenu(hMenuMain, MF_POPUP, (UINT_PTR)hFileMenu, (LPCWSTR)L"File");
+    AppendMenu(hMenuMain, MF_POPUP, (UINT_PTR)hHelpMenu, (LPCWSTR)L"Help");
+    AppendMenu(hHelpMenu, MF_POPUP, ID_ABOUT_MENU, (LPCWSTR)L"About");
+    AppendMenu(hSubFileMenu, MF_STRING, NULL, (LPCWSTR)L"Sub menu   F5");
+
+    AppendMenu(hFileMenu, MF_STRING, NULL, (LPCWSTR)L"New");
+    AppendMenu(hFileMenu, MF_POPUP, (UINT_PTR)hSubFileMenu, (LPCWSTR)L"Tools");
+    AppendMenu(hFileMenu, MF_SEPARATOR, NULL, NULL);
+    AppendMenu(hFileMenu, MF_STRING, ID_EXIT_MENU, (LPCWSTR)L"Exit");
+
+    SetMenu(hWnd, hMenuMain);
+}
+
+
+
+void handleManu(HWND hWnd) {
+    hMenu = CreateMenu();
+    HMENU hFileMenu = CreateMenu();
+
+    AppendMenu(hFileMenu, MF_STRING, ID_EXIT_MENU, (LPCWSTR)L"Exit");
+
+    AppendMenu(hMenu, MF_STRING, (UINT_PTR)hFileMenu, (LPCWSTR)L"File");
+
+    SetMenu(hWnd, hMenu);
+}
+
+void showManuDistructionFailed() {
+    MessageBox(
+        NULL,
+        (LPCWSTR)L"Manu Distruction Failed.",
+        (LPCWSTR)L"distruction info.",
+        MB_ICONWARNING
+    );
+}
+
+void handleDistroyManu() {
+    if (hMenu) {
+        int manuDestroyed = DestroyMenu(hMenu);
+        if (!manuDestroyed) showManuDistructionFailed();
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+void handleNewWidow(HWND hWnd) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//cetering window
+void handleCenterWindow(HWND hWnd) {
+    int centerd = CenterWindow(hWnd);
+    if (!centerd) DisplayWindowCannotMoveMessageBox(hWnd);
+}
+
+
+void handleButton(HWND hWnd) {
+    //button
+    HWND hwndButton = CreateWindow(
+        L"BUTTON",  // Predefined class; Unicode assumed 
+        L"OK",      // Button text 
+        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,  // Styles 
+        10,         // x position 
+        10,         // y position 
+        100,        // Button width
+        30,        // Button height
+        hWnd,     // Parent window
+        NULL,       // No menu.
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+        NULL);      // Pointer not needed.
+
+
+    if (!hwndButton)
+    {
+        MessageBox(NULL,
+            _T("Call to CreateButton failed!"),
+            _T("Windows Desktop Guided Tour"),
+            NULL);
+    }
+}
+
+
